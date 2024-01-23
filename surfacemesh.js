0b6918bf83ff3941f21908b77e947064802022e0
lingo <boeing.chen@gmail.com>
2024-01-23 13:55:15 +0000

    Refactor HoleArray, don't allocated extra memory if there is no Hole.
    FossilOrigin-Name: 6bf4347c8da42e7f71e18adea148c1676402015b9559dda5aa2cbd6c91f231ae

--- surfacemesh.js
+++ surfacemesh.js
@@ -1495,11 +1495,17 @@ class HoleArray {
    // int32(4 bytes) * length.
    //
    computeBufferSize(length) {
-      return this._holes.structSize()*(length+1);     // added sentinel
+      if (length) {
+         return this._holes.structSize()*(length+1);     // added sentinel
+      }
+      return 0;
    }
    
    setBuffer(bufferInfo, byteOffset, length) {
-      length++;                                       // remember to add sentinel
+      if (length) {
+         length++;                                       // remember to add sentinel
+      }
+      
       if (!bufferInfo) {
          bufferInfo = allocBuffer(this.computeBufferSize(length));
          byteOffset = 0;
